523ada03808fc52eb9280aec26dbbbba26c70812
mehekpun <mehekpun665@gmail.com>
2026-03-06 14:12:31 +0000

    done

---
+++ 02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";
+
+//alert(3+3) we r using node js not browser
+
+//ecma decides rules and regulations for writing js
+
+//datatypes
+
+let name = "mehek"
+let age = 18
+let n = false
+
+//number 2^53-1
+//bigint if too big in trading stockmarket etc
+//string
+//boolean
+//null -> stand alone value, it defined and declared empty
+//undefined -> var is declared but no value
+//symbol -> unique
+
+
+//object
+
+console.log(typeof null)
+console.log(typeof undefined)
+
+//null is an object, but undefined is an type
---
+++ variable_01.js
@@ -0,0 +1,10 @@
+const accid = 12
+let a="m"
+var m = "a"
+city = "pune"
+let acc
+console.table([accid,a,m,city,acc])
+
+/* prefer not to use var*/
+
+
