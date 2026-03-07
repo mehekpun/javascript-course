21f0392136999a0dd43d76f110f4a70ea390c878
mehekpun <mehekpun665@gmail.com>
2026-03-07 11:01:00 +0000

    hi

--- 04_comparision.js
+++ 04_comparision.js
@@ -1,2 +1,14 @@
-console.log("2">1)
-console.log("02">1)
+//console.log("2">1)
+//console.log("02">1)
+
+console.log(null>0) //false
+console.log(null>=0) //true
+console.log(null==0) //false
+
+//== and comparision check operators work differently
+//comparisions convert null to 0 and iss liye null>=0 is true but null>0 is false
+
+//undefined is false for all
+
+console.log("2"==2)
+console.log("2"===2) //will check datatype as well
