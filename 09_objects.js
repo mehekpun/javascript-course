7afda7146a2e0141a6e03b50aef9d4955e8853e3
mehekpun <mehekpun665@gmail.com>
2026-03-22 09:42:10 +0530

    yes

--- 09_objects.js
+++ 09_objects.js
@@ -5,11 +5,28 @@
 
 //object literals 
 
+const mysym = Symbol("key1")
 const jsuser = {
     name: "mehek", //key:value
     age : 19,
-    location: "pune"
-
+    location: "pune",
+    [mysym]: "mykey1" //tp use symbol we use []brackets.
 } //object
 
+console.log(jsuser["name"])
 console.log(jsuser.name)
+console.log(jsuser[mysym])
+jsuser.age = 20
+console.log(jsuser.age)
+
+//Object.freeze(jsuser)//the values won't change
+
+jsuser.greeting = function(){
+    console.log("hello js user")
+}
+jsuser.greetingtwo = function(){
+    console.log(`hello js user, ${this.name}`)
+}
+console.log(jsuser.greeting())
+console.log(jsuser.greeting) //function return back - function execute nhi hua hai bs reference aaya hai
+console.log(jsuser.greetingtwo())
